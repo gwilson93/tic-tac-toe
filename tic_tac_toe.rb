a759adcd612b9190cde039d05d6c21655cd0b836
Garret Wilson <60952801+gwilson93@users.noreply.github.com>
2023-11-17 15:15:53 -0500

    Finish instructions and initial game board layout

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -3,21 +3,23 @@
 # Methods for printing out different instructions in the game
 module Instructions
   def startup_msg
-    puts "\nWelcome to Tic-Tac-Toe!"
+    puts "\nTic-Tac-Toe!"
+    puts "\nWelcome to tic-tac-toe. The rules are as expected, but choosing placement requires coordinates."
+    puts 'Each turn, enter two numbers with a space, per the grid layout below:'
+    puts "\r\n"
+    puts '0 0 | 0 1 | 0 2'
+    puts '1 0 | 1 1 | 1 2'
+    puts '2 0 | 2 1 | 2 2'
+    puts "\r\n"
   end
 end
 
+# Game class to play game of tic tac toe
 class Game
   include Instructions
   def playgame
     startup_msg
   end
-end
-
-class Player
-end
-
-class Board
 end
 
 game = Game.new
